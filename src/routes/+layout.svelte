b8114f51a3d9b0d84e78498b0bd0730c94f9672c
Danny Wardwell <dwardwell1@gmail.com>
2024-02-25 18:55:14 -0800

    set up initial fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
+</svelte:head>
+
+<svelte:body class="cinzel-400" />
+<slot />
+
+
+<style>
+
+:global(body) {
+    font-family: "Cinzel", serif;
+    background-color: #224;
+    color: #bbb;
+    display: grid;
+    place-content: center;
+    height: 50vh;
+}
+
+:global(.cinzel-400) {
+  font-family: "Cinzel", serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
