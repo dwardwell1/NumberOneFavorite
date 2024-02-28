bf4db77aed322fe394885693f8bcd0d84181ef6b
Rory Duncan <robust.rory@gmail.com>
2024-02-27 22:17:33 -0800

    Add TODO list to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2>Features</h2>
+<ol>
+    <li>Authentication w/ Google Sign-in</li>
+    <li>Authorized Routes</li>
+    <li>Navigation</li>
+    <li>Better Font (if one even exists)</li>
+    <li>Landing Page / Feed</li>
+    <li>Improved Form Experience</li>
+    <li>Likes / Youtube API</li>
+    <li>Design System</li>
+</ol>
+
+<h2>Pages</h2>
+<ul>
+    <li>Login</li>
+    <li>Logout</li>
+    <li>About</li>
+    <li>Feed / Landing</li>
+    <li>Account</li>
+    <li>My Favorites</li>
+    <li>Add Favorite</li>
+</ul>
